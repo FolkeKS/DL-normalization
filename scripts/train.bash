15a75923689816485a992e2fb744a8112f832b8d
FolkeKS <skrunes@cerfacs.fr>
2022-06-07 12:09:47 +0200

    Move slurm outputs to dedicated folder

--- scripts/train.bash
+++ scripts/train.bash
@@ -4,6 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name train_unet
 #SBATCH --time=12:00:00
+#SBATCH --output=results/outputs/slurm-%A.%a.out
 unset PYTHONHOME
 
 srun python scripts/trainer.py fit --config configs/kraken_demo.yaml> results/outputs/${SLURM_JOBID}.out
